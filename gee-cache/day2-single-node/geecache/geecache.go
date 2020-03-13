70d6bf8fb6ceed37fd5796aec9298ab51569f8f9
mcrwayfun <mcrwayfun@gmail.com>
2020-03-13 23:50:06 +0800

    单机并发缓存 完成

--- gee-cache/day2-single-node/geecache/geecache.go
+++ gee-cache/day2-single-node/geecache/geecache.go
@@ -1,5 +1,11 @@
 package geecache
 
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
 // A Getter loads data for a key
 type Getter interface {
 	Get(key string) ([]byte, error)
@@ -11,4 +17,79 @@ type GetterFunc func(key string) ([]byte, error)
 // Get implements Getter interface function
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
+}
+
+// A Group is a cache namespace and associated data loaded spread over
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// NewGroup create a new instance of Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+
+	groups[name] = g
+	return g
+}
+
+// GetGroup returns the named group previously created with NewGroup,or
+// nil if there's no such group
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+// Get value for a key from cache
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	// 已经存在缓存中,则返回
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	// 缓存中不存在,则去加载
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	// 使用回调方法获取数据
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+
+	// 缓存到内存中
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
